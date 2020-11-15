aa2f059a906720e1f7802b1d9993cc8de8c7fa2a
gilangpanjiwp2197 <73225699+gilangpanjiwp2197@users.noreply.github.com>
2020-11-15 11:04:41 +0700

    Add files via upload

---
+++ PBO2-10116417-Latihan45-CetakNama/src/pbo2/pkg10116417/latihan45/cetaknama/PBO210116417Latihan45CetakNama.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10116417.latihan45.cetaknama;
+import java.util.Scanner;
+
+/**
+ *
+ * @author
+ *  NAMA        : GILANG PANJI WANENG PATI
+ *  KELAS       : PBO2
+ *  NIM         : 10116417
+ *  DESKRIPSI   : BERISI TENTANG PROGRAM MENAMPILKAN CETAK NAMA
+ */
+public class PBO210116417Latihan45CetakNama {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner scan1 = new Scanner(System.in);
+        Scanner scan2 = new Scanner(System.in);
+
+        Printer objP = new Printer();
+
+        System.out.println("=====Aplikasi Pencetak Nama=====");
+        System.out.print("Masukan Nama Anda : ");
+        objP.setNama(scan1.nextLine());
+        System.out.print("Mau Cetak Berapa Kali? : ");
+        objP.setJmlCetak(scan2.nextInt());
+        objP.cetak(objP.getNama());
+        System.out.println("Hasil Cetak : ");
+        objP.cetak(objP.getJmlCetak(), objP.getNama());
+    }
+    
+}
